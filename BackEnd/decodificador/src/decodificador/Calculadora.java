2d30295f20e0e05d0d4f3ec68986ffa5be099280
[afernan3] <[antonio.fernandez-tomas@capgemini.com]>
2022-01-31 13:36:30 +0100

    calculadora al 50%

---
+++ BackEnd/decodificador/src/decodificador/Calculadora.java
@@ -0,0 +1,6 @@
+package decodificador;
+
+public class Calculadora {
+
+	
+}
--- BackEnd/decodificador/src/decodificador/Decodificador.java
+++ BackEnd/decodificador/src/decodificador/Decodificador.java
@@ -1,16 +1,32 @@
 package decodificador;
 
+import java.util.Arrays;
+
 public class Decodificador {
 
 	public static void main(String[] args) {
 		
-		String cadena = "3+4+3,4-7*1=";
-		String[] partes = cadena.split("(?<=[-+*=])");
-
+		int num1,num2;
+		String op;
 		
-		for (int i = 0; i < partes.length; i++) {
-			System.out.println(partes[i]);
+		String cadena = "3+4+3,4-7*1=";
+	
+		int i;
+		char[] subCadenas = cadena.toCharArray();
+		String num = "";
+		
+		for( i=0; i< subCadenas.length; i++) {
+			if(Character.isDigit(subCadenas[i])) {
+				num += subCadenas[i];
+				System.out.println(Arrays.toString(subCadenas));
+			}else {
+				System.out.println(num+ " " + subCadenas[i]);
+				num="";
+			}
 		}
+		
+		//para mostrar el array literal
+		//System.out.println(Arrays.toString(partes));
 		
 	}
 
