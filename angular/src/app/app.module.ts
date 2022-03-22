5dd06e51976d6075c83d6d6f114fbfb0698706f2
Antonio <antoniofernandeztomas@gmail.com>
2022-03-22 14:02:30 +0100

    cambios angular

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -9,8 +9,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+    BrowserModule, FormsModule,
+    AppRoutingModule, MainModule, SecurityModule, MyCoreModule, 
   ],
   providers: [],
   bootstrap: [AppComponent]
